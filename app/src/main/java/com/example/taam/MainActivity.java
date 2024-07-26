1d6ada1cf1adf8a00c87f83b634a84005913d9b5
Tornadoguy26 <prithviajay100@gmail.com>
2024-07-25 23:51:23 -0400

    feat: Created login screen

--- app/src/main/java/com/example/taam/MainActivity.java
+++ app/src/main/java/com/example/taam/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.taam;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button adminBTN;
+    Dialog logindialog;
+    Button adminCancelBTN, adminLoginBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        adminBTN = findViewById(R.id.AdminLogin);
+
+        logindialog = new Dialog(this);
+        logindialog.setContentView(R.layout.login_screen);
+        logindialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        logindialog.setCancelable(false);
+
+        adminCancelBTN = logindialog.findViewById(R.id.BackButton);
+        adminLoginBTN = logindialog.findViewById(R.id.LogButton);
+
+        adminBTN.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                logindialog.show();
+            }
+        });
+
+        adminCancelBTN.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               logindialog.dismiss();
+           }
+        });
+
+        adminLoginBTN.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
         });
     }
 }
