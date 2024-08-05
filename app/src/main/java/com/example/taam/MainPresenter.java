dd2dd0c6f12dee8218981d49d02ef5d327f60c54
Bobby Adi Salim <13obby1025@gmail.com>
2024-08-05 16:58:24 -0400

    temp

--- app/src/main/java/com/example/taam/MainPresenter.java
+++ app/src/main/java/com/example/taam/MainPresenter.java
@@ -137,7 +137,9 @@ public class MainPresenter {
             return;
         }
 
-        for (int i = 0; i < items.size(); i++) {
+
+
+        for (int i = 0; i < items.size() - 1; i++) {
             PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, i + 1).create();
             PdfDocument.Page page = pdfDocument.startPage(pageInfo);
             Item item = items.get(i);
@@ -177,81 +179,6 @@ public class MainPresenter {
             Log.d(TAG, "image: " + filename);
             StorageReference imageRef = storageReference.child(filename);
 
-            // This code is commented out because it is not working
-            // Tried to check if file exists or not, then add it to the pdf
-            /*
-            imageRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
-                @Override
-                public void onSuccess(StorageMetadata metadata) {
-                    Log.d("MainPresenter", "File exists");
-
-                    try {
-                        File localFile = File.createTempFile("images", "jpg");
-                        Log.d("MainPresenter", "Temp file created");
-                        imageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                                // File exists
-                                Log.d("MainPresenter", "Download success");
-                                // Decode the file into a Bitmap
-                                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                                if (bitmap == null) {
-                                    Log.d("MainPresenter", "Failed to decode image");
-                                }
-                                // Generate the PDF with the downloaded image
-                                Paint paint_img = new Paint();
-                                canvas.drawBitmap(bitmap, 10, y_img, paint_img);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(Exception exception) {
-                                Log.e("MainPresenter", "Image download failed", exception);
-                            }
-                        });
-                    } catch (IOException e) {
-                        Log.e("MainPresenter", "Error creating temp file", e);
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    if (exception instanceof StorageException) {
-                        StorageException se = (StorageException) exception;
-                        if (se.getErrorCode() == StorageException.ERROR_OBJECT_NOT_FOUND) {
-                            Log.e("MainPresenter", "File not found at specified path");
-                            // Handle file not found case
-                        } else {
-                            Log.e("MainPresenter", "Failed to get metadata", exception);
-                            // Handle other errors
-                        }
-                    }
-                }
-            });
-
-             */
-
-            // This code is commented out because it is also not working :D
-            // Tried to make a function to get the image from the storage reference
-            // Thought it was a synchronization issue, waited for each image to load
-            // But the image won't load at all
-            // Even with trivial cases; with 1.png
-            /*
-            Bitmap bitmap;
-            try {
-                bitmap = getBitmapFromStorageReference(imageRef);
-                Paint paint_img = new Paint();
-                canvas.drawBitmap(bitmap, 10, y_img, paint_img);
-            } catch (ExecutionException | InterruptedException e) {
-                Log.e(TAG, "Failed to load image", e);
-            }
-
-             */
-
-            // I don't understand why the image always fail to load
-            // This is a simple implementation I tried but also not working
-            // As I wrote previously, I think it is a sync issue
-            // But I don't know how to fix it
-            /*
             storageReference.getBytes(1024 * 1024)
                     .addOnSuccessListener(new OnSuccessListener<byte[]>() {
                         @Override
@@ -260,6 +187,7 @@ public class MainPresenter {
                             Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                             Paint paint_img = new Paint();
                             canvas.drawBitmap(bitmap, 10, y_img, paint_img);
+                            pdfDocument.finishPage(page);
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
@@ -268,78 +196,107 @@ public class MainPresenter {
                             Log.e(TAG, "Failed to load image", exception);
                         }
                     });
+        }
+        // Last page
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, items.size()).create();
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+        Item item = items.get(items.size()-1);
 
-             */
-            pdfDocument.finishPage(page);
+        Canvas canvas = page.getCanvas();
+        if (canvas == null) {
+            Log.e(TAG, "Failed to create canvas");
         }
 
-        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-        File file = new File(pdfPath, "TAAM.pdf");
-        int counter = 0;
-        while (file.exists()) {
-            counter++;
-            file = new File(pdfPath, "TAAM(" + counter + ").pdf");
-        }
+        canvas.drawText("Lot Number: " + item.getLotNumber(), 10, 25, paint);
+        canvas.drawText("Name: " + item.getName(), 10, 50, paint);
+        canvas.drawText("Category: " + item.getCategory(), 10, 75, paint);
+        canvas.drawText("Period: " + item.getPeriod(), 10, 100, paint);
 
-        try {
-            // writing our PDF file to that location.
-            pdfDocument.writeTo(Files.newOutputStream(file.toPath()));
-            Log.d(TAG, "PDF file written");
-            // printing toast message on completion of PDF generation.
-            Toast.makeText(mainActivity, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            // handling error
-            Toast.makeText(mainActivity, "Failed to generate PDF file.", Toast.LENGTH_SHORT).show();
-        }
-
-        // closing our PDF file.
-        pdfDocument.close();
-        Log.d(TAG, "PDF file closed");
-    }
-
-    /*
-    public static Bitmap getBitmapFromStorageReference(StorageReference storageReference) throws ExecutionException, InterruptedException {
-        Log.d(TAG, "Loading image from storage reference");
-        Callable<Bitmap> task = new Callable<Bitmap>() {
-            @Override
-            public Bitmap call() throws Exception {
-                final Bitmap[] result = new Bitmap[1];
-                final Object lock = new Object();
-                Log.d(TAG, "Getting image bytes");
-
-                storageReference.getBytes(1024 * 1024)
-                        .addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                            @Override
-                            public void onSuccess(byte[] bytes) {
-                                Log.d(TAG, "Successfully loaded image");
-                                result[0] = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                synchronized (lock) {
-                                    lock.notify();
-                                }
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                Log.e(TAG, "Failed to load image", exception);
-                                synchronized (lock) {
-                                    lock.notify();
-                                }
-                            }
-                        });
-
-                synchronized (lock) {
-                    Log.d(TAG, "Waiting");
-                    lock.wait(); // Wait for the image to be loaded
-                }
-                return result[0];
+        // Multi-line description
+        String[] desc = item.getDescription().split(" ");
+        StringBuilder line = new StringBuilder();
+        int y = 125;
+        for (String word : desc) {
+            float textWidth = paint.measureText(line + word + " ");
+            if (textWidth <= pageWidth - 10) {
+                line.append(word).append(" ");
+            } else {
+                canvas.drawText(line.toString(), 10, y, paint);
+                line = new StringBuilder(word).append(" ");
+                y += 25;
             }
-        };
+        }
+        if (!line.toString().isEmpty()) {
+            canvas.drawText(line.toString(), 10, y, paint);
+        }
 
-        Future<Bitmap> future = executorService.submit(task);
-        Log.d(TAG, "Waiting for image to load");
-        return future.get(); // This will block until the task is complete
+        final int y_img = y + 25;
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        String filename = item.getLotNumber() + ".png";
+        Log.d(TAG, "image: " + filename);
+        StorageReference imageRef = storageReference.child(filename);
+
+        storageReference.getBytes(1024 * 1024)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Log.d(TAG, "Successfully loaded image");
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        Paint paint_img = new Paint();
+                        canvas.drawBitmap(bitmap, 10, y_img, paint_img);
+                        pdfDocument.finishPage(page);
+                        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                        File file = new File(pdfPath, "TAAM.pdf");
+                        int counter = 0;
+                        while (file.exists()) {
+                            counter++;
+                            file = new File(pdfPath, "TAAM(" + counter + ").pdf");
+                        }
+
+                        try {
+                            // writing our PDF file to that location.
+                            pdfDocument.writeTo(Files.newOutputStream(file.toPath()));
+                            Log.d(TAG, "PDF file written");
+                            // printing toast message on completion of PDF generation.
+                            Toast.makeText(mainActivity, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            // handling error
+                            Toast.makeText(mainActivity, "Failed to generate PDF file.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // closing our PDF file.
+                        pdfDocument.close();
+                        Log.d(TAG, "PDF file closed");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pdfDocument.finishPage(page);
+                        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                        File file = new File(pdfPath, "TAAM.pdf");
+                        int counter = 0;
+                        while (file.exists()) {
+                            counter++;
+                            file = new File(pdfPath, "TAAM(" + counter + ").pdf");
+                        }
+
+                        try {
+                            // writing our PDF file to that location.
+                            pdfDocument.writeTo(Files.newOutputStream(file.toPath()));
+                            Log.d(TAG, "PDF file written");
+                            // printing toast message on completion of PDF generation.
+                            Toast.makeText(mainActivity, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            // handling error
+                            Toast.makeText(mainActivity, "Failed to generate PDF file.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // closing our PDF file.
+                        pdfDocument.close();
+                        Log.d(TAG, "PDF file closed");
+                    }
+                });
     }
-
-     */
 }
