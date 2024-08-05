eb57a3bb2f982e356387400fa5017d72079d50f6
Bobby Adi Salim <13obby1025@gmail.com>
2024-08-05 18:27:50 -0400

    feat: bugfix done; generatePdf working as wanted

--- app/src/main/java/com/example/taam/MainPresenter.java
+++ app/src/main/java/com/example/taam/MainPresenter.java
@@ -15,6 +15,9 @@ import com.example.taam.structures.Item;
 import com.example.taam.structures.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
@@ -55,14 +58,9 @@ public class MainPresenter {
     public void generateReport(String searchBy, String searchValue, boolean descPic, ArrayList<Item> itemDataSet) {
         ArrayList<Item> items = new ArrayList<>(itemDataSet);
 
-        int pageHeight = 1120;
-        int pageWidth = 792;
-
         // Check if file already exists and create a unique filename
 
         Toast.makeText(mainActivity, "Generating report", Toast.LENGTH_SHORT).show();
-        PdfDocument pdfDocument = new PdfDocument();
-        Paint paint = new Paint();
 
         switch (searchBy) {
             case "Lot Number":
@@ -80,75 +78,81 @@ public class MainPresenter {
         }
 
         if (descPic) {
+            List<Task<Void>> tasks = new ArrayList<>();
             ArrayList<Integer> indices = new ArrayList<>();
-            for (final int[] i = {0}; i[0] < items.size(); i[0]++) {
-                Item item = items.get(i[0]);
+            for (int i = 0; i < items.size(); i++) {
+                Item item = items.get(i);
                 StorageReference storageReference = FirebaseStorage.getInstance().getReference();
                 StorageReference imageFile = storageReference.child(item.getLotNumber() + ".png");
-
-                if (item.getDescription() == null) {
-                    indices.add(i[0]);
-                    continue;
+                Task<Void> task = removeIfNoImage(indices, i, imageFile);
+                tasks.add(task);
+            }
+            Tasks.whenAll(tasks).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "All async functions are done.");
+                    for (Integer index : indices) {
+                        Log.d(TAG, "Removing item at index " + index);
+                        items.remove(items.get(index));
+                    }
+                    writePdf(items);
+                } else {
+                    Log.e(TAG, "Some async functions failed.", task.getException());
                 }
-
-                imageFile.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
-                    @Override
-                    public void onSuccess(StorageMetadata metadata) {
-                        // File exists
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // File does not exist
-                        indices.add(i[0]);
-                    }
-                });
-            }
-            for (Integer index : indices) {
-                items.remove(items.get(index));
-            }
+            });
         }
 
+        writePdf(items);
+    }
+
+    public void writePdf(ArrayList<Item> items) {
         if (items.isEmpty()) {
             Toast.makeText(mainActivity, "No items found", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        Log.d(TAG, "Number of items: " + items.size());
+
         final Bitmap[] bitmaps = new Bitmap[items.size()];
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        List<Task<Void>> tasks = new ArrayList<>();
 
         for (int i = 0; i < items.size(); i++) {
             Item item = items.get(i);
             StorageReference storageReference = FirebaseStorage.getInstance().getReference();
             StorageReference imageRef = storageReference.child(item.getLotNumber() + ".png");
-            final int finalI = i;
-            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> getBitmap(bitmaps, finalI, imageRef));
-            futures.add(future);
+            Task<Void> task = getBitmap(bitmaps, i, imageRef);
+            tasks.add(task);
         }
 
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        allOf.thenRun(() -> {
-            Log.d(TAG, "All async functions are done.");
-            for (int i = 0; i < items.size(); i++) {
-                Log.d(TAG, "Creating page " + (i + 1));
-                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, i + 1).create();
-                PdfDocument.Page page = pdfDocument.startPage(pageInfo);
-                Item item = items.get(i);
+        PdfDocument pdfDocument = new PdfDocument();
+        Paint paint = new Paint();
 
-                Canvas canvas = page.getCanvas();
-                if (canvas == null) {
-                    Log.e(TAG, "Failed to create canvas");
-                }
+        int pageHeight = 1120;
+        int pageWidth = 792;
 
-                canvas.drawText("Lot Number: " + item.getLotNumber(), 10, 25, paint);
-                canvas.drawText("Name: " + item.getName(), 10, 50, paint);
-                canvas.drawText("Category: " + item.getCategory(), 10, 75, paint);
-                canvas.drawText("Period: " + item.getPeriod(), 10, 100, paint);
+        Tasks.whenAll(tasks).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // All tasks completed successfully
+                Log.d("MainPresenter", "All downloads completed successfully.");
+                Log.d(TAG, "All async functions are done.");
+                for (int i = 0; i < items.size(); i++) {
+                    Log.d(TAG, "Creating page " + (i + 1));
+                    PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, i + 1).create();
+                    PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+                    Item item = items.get(i);
 
-                // Multi-line description
-                int y = 125;
-                String description = item.getDescription();
-                if (description != null) {
+                    Canvas canvas = page.getCanvas();
+                    if (canvas == null) {
+                        Log.e(TAG, "Failed to create canvas");
+                    }
+
+                    canvas.drawText("Lot Number: " + item.getLotNumber(), 10, 25, paint);
+                    canvas.drawText("Name: " + item.getName(), 10, 50, paint);
+                    canvas.drawText("Category: " + item.getCategory(), 10, 75, paint);
+                    canvas.drawText("Period: " + item.getPeriod(), 10, 100, paint);
+
+                    // Multi-line description
+                    int y = 125;
+                    String description = item.getDescription();
                     String[] desc = item.getDescription().split(" ");
                     StringBuilder line = new StringBuilder();
                     for (String word : desc) {
@@ -164,108 +168,94 @@ public class MainPresenter {
                     if (!line.toString().isEmpty()) {
                         canvas.drawText(line.toString(), 10, y, paint);
                     }
-                }
 
-                if (bitmaps[i] != null) {
-                    canvas.drawBitmap(bitmaps[i], 10, y + 25, paint);
-                } else {
-                    Log.d(TAG, "No image available");
-                    canvas.drawText("No image available", 10, y + 25, paint);
-                }
-
-                pdfDocument.finishPage(page);
-                Log.d(TAG, "Page " + (i + 1) + " created");
-            }
-            Log.d(TAG, "all pages done");
-
-            String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-            File file = new File(pdfPath, "TAAM.pdf");
-            int counter = 0;
-            while (file.exists()) {
-                counter++;
-                file = new File(pdfPath, "TAAM(" + counter + ").pdf");
-            }
-
-            try {
-                // writing our PDF file to that location.
-                pdfDocument.writeTo(Files.newOutputStream(file.toPath()));
-                Log.d(TAG, "PDF file written");
-                // printing toast message on completion of PDF generation.
-                Toast.makeText(mainActivity, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                // handling error
-                Toast.makeText(mainActivity, "Failed to generate PDF file.", Toast.LENGTH_SHORT).show();
-            }
-
-            // closing our PDF file.
-            pdfDocument.close();
-            Log.d(TAG, "PDF file closed");
-        });
-    }
-
-    public static void getBitmap(final Bitmap[] bitmaps, final int finalI, StorageReference imageRef) {
-        imageRef.getBytes(1024 * 1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                bitmaps[finalI] = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                bitmaps[finalI] = null;
-                Log.e(TAG, "No image", e);
-            }
-        });
-    }
-}
-
-
-/*
-imageRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
-                @Override
-                public void onSuccess(StorageMetadata metadata) {
-                    Log.d("MainPresenter", "File exists");
-
-                    try {
-                        File localFile = File.createTempFile("images", "jpg");
-                        Log.d("MainPresenter", "Temp file created");
-                        imageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                                // File exists
-                                Log.d("MainPresenter", "Download success");
-                                // Decode the file into a Bitmap
-                                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                                if (bitmap == null) {
-                                    Log.d("MainPresenter", "Failed to decode image");
-                                }
-                                // Generate the PDF with the downloaded image
-                                Paint paint_img = new Paint();
-                                canvas.drawBitmap(bitmap, 10, y_img, paint_img);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(Exception exception) {
-                                Log.e("MainPresenter", "Image download failed", exception);
-                            }
-                        });
-                    } catch (IOException e) {
-                        Log.e("MainPresenter", "Error creating temp file", e);
+                    if (bitmaps[i] != null) {
+                        canvas.drawBitmap(bitmaps[i], 10, y + 25, paint);
+                    } else {
+                        Log.d(TAG, "No image available");
+                        canvas.drawText("No image available", 10, y + 25, paint);
                     }
+
+                    pdfDocument.finishPage(page);
+                    Log.d(TAG, "Page " + (i + 1) + " created");
+                }
+                Log.d(TAG, "all pages done");
+
+                String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                File file = new File(pdfPath, "TAAM.pdf");
+                int counter = 0;
+                while (file.exists()) {
+                    counter++;
+                    file = new File(pdfPath, "TAAM(" + counter + ").pdf");
+                }
+
+                try {
+                    // writing our PDF file to that location.
+                    pdfDocument.writeTo(Files.newOutputStream(file.toPath()));
+                    Log.d(TAG, "PDF file written");
+                    // printing toast message on completion of PDF generation.
+                    Toast.makeText(mainActivity, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    // handling error
+                    Toast.makeText(mainActivity, "Failed to generate PDF file.", Toast.LENGTH_SHORT).show();
+                }
+
+                // closing our PDF file.
+                pdfDocument.close();
+                Log.d(TAG, "PDF file closed");
+            } else {
+                // Handle failures
+                Log.e("MainPresenter", "Some downloads failed.", task.getException());
+            }
+        });
+    }
+
+    public static Task<Void> getBitmap(final Bitmap[] bitmaps, final int finalI, StorageReference imageRef) {
+        TaskCompletionSource<Void> taskCompletionSource = new TaskCompletionSource<>();
+        try {
+            File localFile = File.createTempFile("images", "jpg");
+            Log.d("MainPresenter", "Temp file created");
+            imageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    // File exists
+                    Log.d("MainPresenter", "Download success");
+                    // Decode the file into a Bitmap
+                    bitmaps[finalI] = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    taskCompletionSource.setResult(null); // Mark the task as complete
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
-                public void onFailure(@NonNull Exception exception) {
-                    if (exception instanceof StorageException) {
-                        StorageException se = (StorageException) exception;
-                        if (se.getErrorCode() == StorageException.ERROR_OBJECT_NOT_FOUND) {
-                            Log.e("MainPresenter", "File not found at specified path");
-                            // Handle file not found case
-                        } else {
-                            Log.e("MainPresenter", "Failed to get metadata", exception);
-                            // Handle other errors
-                        }
-                    }
+                public void onFailure(Exception exception) {
+                    bitmaps[finalI] = null;
+                    Log.e(TAG, "No image", exception);
+                    taskCompletionSource.setResult(null); // Mark the task as complete
                 }
             });
- */
+        } catch (IOException e) {
+            bitmaps[finalI] = null;
+            Log.e("MainPresenter", "Error creating temp file", e);
+            taskCompletionSource.setException(e); // Mark the task as failed
+        }
+        return taskCompletionSource.getTask();
+    }
+
+    public static Task<Void> removeIfNoImage(ArrayList<Integer> indices, final int finalI, StorageReference imageRef) {
+        TaskCompletionSource<Void> taskCompletionSource = new TaskCompletionSource<>();
+        imageRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+            @Override
+            public void onSuccess(StorageMetadata metadata) {
+                // File exists
+                taskCompletionSource.setResult(null); // Mark the task as complete
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // File does not exist
+                indices.add(finalI);
+                taskCompletionSource.setResult(null); // Mark the task as complete
+            }
+        });
+        return taskCompletionSource.getTask();
+    }
+}
